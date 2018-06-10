85f849ec26ac7723f897b3cd1f37d4d4d37fe031
sahadatHossen <sahadat@encodersbd.com>
2018-06-10 13:07:18 +0600

    Project Creation

---
+++ QmsApp/QmsApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QmsApp.Startup))]
+namespace QmsApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
