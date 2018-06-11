0f56a9bf6e045211b80c11a96e84216f2bba1fb0
sahadatHossen <sahadat@encodersbd.com>
2018-06-11 13:58:30 +0600

    Layout Complete

--- QmsApp/QmsApp/App_Start/BundleConfig.cs
+++ QmsApp/QmsApp/App_Start/BundleConfig.cs
@@ -8,24 +8,40 @@ namespace QmsApp
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
+          bundles.Add(new StyleBundle("~/Content/css").Include(
+              "~/css/bootstrap.min.css",
+              "~/css/bootstrap-responsive.min.css",
+              "~/css/maruti-style.css",
+              "~/css/maruti-media.css"
+              ));
+            //canvas
+    
+            //bootstrap
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                "~/js/bootstrap.min.js")); 
+            //flot
+            bundles.Add(new ScriptBundle("~/bundles/flot").Include(
+                "~/js/jquery.flot.min.js",
+                "~/js/jquery.flot.resize.min.js",
+                "~/js/jquery.flot.pie.min.js"
+                )); 
+            //js
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                "~/js/jquery.min.js",
+                "~/js/maruti.js"
+               
+                ));  
+            //chart
+            bundles.Add(new ScriptBundle("~/bundles/chart").Include(
+              
+               "~/js/maruti.charts.js",
+                 "~/js/jquery.ui.custom.js"
+                ));
+            bundles.Add(new ScriptBundle("~/bundles/canvas").Include(
+        "~/js/excanvas.min.js",
+          "~/js/jquery.peity.min.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+
         }
     }
 }
