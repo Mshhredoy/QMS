f4149dda75a701e9e0cab1556e7efa6405e3b36f
sahadatHossen <sahadat@encodersbd.com>
2018-06-11 16:04:20 +0600

    Entity Set

--- QmsApp/QmsApp/Controllers/HomeController.cs
+++ QmsApp/QmsApp/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using QmsApp.Models;
 
 namespace QmsApp.Controllers
 {
@@ -10,6 +12,7 @@ namespace QmsApp.Controllers
     {
         public ActionResult Dashboard()
         {
+            
             return View();
         }
 
